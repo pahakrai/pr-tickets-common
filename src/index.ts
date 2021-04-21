aabe10a48a197a181f42b361564d0128fdcc1114
pahakrai <pahak@httpeace.com>
2021-04-21 18:54:47 +0800

    Updates

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+// comment add
